b99a769c280806ea0dc5d30c5c58b542ff8b30f0
NeilDG <fm.atropos@gmail.com>
2019-07-17 21:41:49 +0800

    Stable refactor checkpoint.

--- src/model/warp_cnn.py
+++ src/model/warp_cnn.py
@@ -18,18 +18,18 @@ class WarpCNN(nn.Module):
         self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size=7, stride=3, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=6, stride=2, padding=0)
         
-        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 32, kernel_size = 5, stride = 3, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 5, stride = 3, padding = 1)
         self.pool2 = nn.MaxPool2d(kernel_size=5, stride=2, padding=0)
         
-        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, stride = 3, padding = 1)
+        self.conv3 = nn.Conv2d(in_channels = 64, out_channels = 48, kernel_size = 3, stride = 3, padding = 1)
         self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
         
-        self.conv4 = nn.Conv2d(in_channels = 32, out_channels = 16, kernel_size = 2, stride = 2, padding = 1)
+        self.conv4 = nn.Conv2d(in_channels = 48, out_channels = 24, kernel_size = 2, stride = 2, padding = 1)
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        self.conv5 = nn.Conv2d(in_channels = 16, out_channels = 8, kernel_size = 2, stride = 2, padding = 1)
+        self.conv5 = nn.Conv2d(in_channels = 24, out_channels = 24, kernel_size = 2, stride = 2, padding = 1)
         
-        self.fc = nn.Linear(16, 1)
+        self.fc = nn.Linear(48, 1)
         
         #init weights
         nn.init.xavier_uniform_(self.conv1.weight)
